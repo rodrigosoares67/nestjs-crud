91a1735508204c12ded2180d435bee425ba3c44d
Rodrigo de Oliveira Carvalho Soares <rodrigo.soares@expocaccer.com.br>
2022-07-08 17:56:27 -0300

    FEAT: Configuração do MongoDB e criação do CRUD de usuários

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,14 @@
 import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UsersModule } from './users/users.module';
 
 @Module({
-  imports: [UsersModule],
+  imports: [
+    MongooseModule.forRoot('mongodb+srv://teste:JUUydhaGSAzZxF2P@cluster0.aee5l.mongodb.net/test'),
+    UsersModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
